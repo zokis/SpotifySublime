cea6ce33e971a04dfcd4433ea30e496a0b263e8c
Marcelo Tambalo <marcelo@znc.com.br>
2016-08-26 18:09:03 -0300

    removes unnecessary garbage

--- Spotify.py
+++ Spotify.py
@@ -1,41 +1,18 @@
-import sublime
 import sublime_plugin
 import subprocess
-import os
 import threading
-
-'''
-alias pnext='dbus-send --print-reply=literal --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next'
-alias pplay='dbus-send --print-reply=literal --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause'
-alias ppause='dbus-send --print-reply=literal --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause'
-alias pprev='dbus-send --print-reply=literal --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous'
-alias pstop='dbus-send --print-reply=literal --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop'
-sadfasdfasdf
-asdfsad
-asdfasdf
-sadfsadfa
-asdfasdfasdf
-
-'''
 
 
 class Runner(threading.Thread):
-
-    BASE = 'dbus-send --print-reply=literal --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.%s'
-
-    def __init__(self, command, shell, env):
+    def __init__(self, command):
         self.stdout = None
         self.stderr = None
         self.command = command or ''
-        self.shell = shell or ''
-        self.env = env or ''
         threading.Thread.__init__(self)
 
     def run(self):
         subprocess.Popen(
             [
-                # self.shell,
-                # '-ic',
                 'dbus-send',
                 '--print-reply=literal',
                 '--dest=org.mpris.MediaPlayer2.spotify',
@@ -54,7 +31,7 @@ class Base(sublime_plugin.WindowCommand):
     command = 'PlayPause'
 
     def run(self):
-        runner = Runner(self.command, os.environ['SHELL'], os.environ.copy())
+        runner = Runner(self.command)
         runner.start()
 
 
